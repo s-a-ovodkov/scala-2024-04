bd72a1a4f653df683ea897b018045e36f747352e
Sergey Ovodkov <ovodkov.s.a@gmail.com>
2024-05-11 12:44:28 +0300

    Реализованы методы Option

--- task01/src/main/scala/sa/ovodkov/task01/opt.scala
+++ task01/src/main/scala/sa/ovodkov/task01/opt.scala
@@ -19,6 +19,23 @@ object opt {
     }
 
     def map[B](f: T => B): Option[B] = flatMap(t => Option(f(t)))
+
+    /**
+     * Реализация метода printIfAny, который будет печатать значение, если оно есть.
+     */
+    def printIfAny: Unit = if (!isEmpty) println(get)
+
+    /**
+     * Реализация метода filter, который будет возвращать не пустой Option
+     * в случае если исходный не пуст и предикат от значения = true
+     */
+    def filter(f: T => Boolean): Option[T] = if (!isEmpty && f(get)) this else None
+
+    /**
+     * Реализация метода zip, который будет создавать Option от пары значений из 2-х Option
+     */
+    def zip[TT >: T, C](other: Option[C]): Option[(TT, C)] =
+      if (isEmpty || other.isEmpty) None else Some(get, other.get)
   }
 
   case class Some[T](v: T) extends Option[T]
@@ -28,19 +45,4 @@ object opt {
   object Option {
     def apply[T](v: T): Option[T] = if (v != null) Some(v) else None
   }
-
-  /**
-   * Реализовать метод printIfAny, который будет печатать значение, если оно есть
-   */
-
-
-  /**
-   * Реализовать метод zip, который будет создавать Option от пары значений из 2-х Option
-   */
-
-
-  /**
-   * Реализовать метод filter, который будет возвращать не пустой Option
-   * в случае если исходный не пуст и предикат от значения = true
-   */
 }
